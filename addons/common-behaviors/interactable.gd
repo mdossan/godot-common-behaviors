6f9765dbc2046ea7ab6c9e81b17fb3fa26638d39
mdos-san <mdossan@protonmail.com>
2025-09-29 12:37:52 +0200

    feat: Add Interactable node

---
+++ addons/common-behaviors/interactable.gd
@@ -0,0 +1,15 @@
+class_name InteractableBehaviour
+extends Area2D
+
+var can_interact = false
+signal interacted(interact_behaviour: CanInteract)
+
+func interract(source: CanInteract):
+	if can_interact:
+		interacted.emit(source)
+
+func _on_area_entered(_area: Area2D) -> void:
+	can_interact = true
+
+func _on_area_exited(_area: Area2D) -> void:
+	can_interact = false
