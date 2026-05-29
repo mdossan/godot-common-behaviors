ffb539e41029a13f603990d946f34dc8bc9813bc
mdossan <mdossan@protonmail.com>
2026-05-29 12:23:17 +0200

    ✅ Split `MdsLifeBehavior` assertions into multiple tests

--- addons/mds_lib/behaviors/life/mds_life_behavior.test.gd
+++ addons/mds_lib/behaviors/life/mds_life_behavior.test.gd
@@ -2,46 +2,37 @@ extends GutTest
 
 var scene = preload("res://addons/mds_lib/behaviors/life/mds_life_behavior.tscn")
 
-func test_life_behavior():
-	# Arrange: Create root node
-	var root_node = Node.new()
-	
-	# Arrange: Create Behavior node
-	var behavior: MdsLifeBehavior = scene.instantiate()
+var root_node: Node
+var behavior: MdsLifeBehavior
+
+func before_each():
+	behavior = scene.instantiate()
+	root_node = Node.new()
 	behavior.parent = root_node
-	behavior.start_life = 50.0
-	watch_signals(behavior)
 	root_node.add_child(behavior)
 	
-	# Arrange: Spawn root_node to the scene
 	add_child_autofree(root_node)
+	watch_signals(behavior)
+
+func test_default_values():
+	assert_eq(behavior.life, 100.0, "Default value should be 100")
 	
-	# Assert: Start with start_life amout
-	assert_eq(behavior.life, 50.0, "Default value should be 50")
-	
-	# Arrange: Reset life to 100
-	behavior.life = 100
-	
-	# Act: Take Damage
+func test_take_damage():
 	behavior.take_damage(20.0)
 	
-	# Assert: Damage taken
 	assert_signal_emitted_with_parameters(behavior.damage_taken, [20.0, 80.0])
 	assert_eq(behavior.life, 80.0, "life should be 80")
-	
-	# Act: Heal
+
+func test_life_behavior():
+	behavior.life = 50.0
 	behavior.heal(10.0)
 	
-	# Assert: Damange taken
-	assert_signal_emitted_with_parameters(behavior.heal_taken, [10.0, 90.0])
-	assert_eq(behavior.life, 90.0, "life should be 90")
-	
-	# Act: Die
-	clear_signal_watcher() # Need to clear because damage_taken already emitted
-	watch_signals(behavior)
+	assert_signal_emitted_with_parameters(behavior.heal_taken, [10.0, 60.0])
+	assert_eq(behavior.life, 60.0, "life should be 60")
+
+func test_death_signal():
 	behavior.take_damage(1000.0)
 	
-	# Assert: Damange taken
 	assert_signal_not_emitted(behavior.damage_taken)
 	assert_signal_emitted_with_parameters(behavior.dead, [])
 	assert_eq(behavior.life, 0.0, "life should be 0")
