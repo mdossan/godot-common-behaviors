92fda96fbaa8a91429c5b50f1340609194985de0
mdossan <mdossan@protonmail.com>
2026-05-31 20:04:03 +0200

    🎨 Improve structure of `MdsStaminaBehavior`

--- addons/mds_lib/behaviors/stamina/mds_stamina_behavior.gd
+++ addons/mds_lib/behaviors/stamina/mds_stamina_behavior.gd
@@ -1,33 +1,35 @@
 @tool
-class_name MdsStaminaBehavior
-extends Node
+class_name MdsStaminaBehavior extends Node
 
-signal stamina_consumed(remaining_stamina: float)
-signal stamina_refilled(current_stamina: float)
-signal not_enough_stamina()
-
+#region Exported Vars
 @export var parent: Node:
 	set(new_value):
 		parent = new_value
 		update_configuration_warnings()
 @export var stamina_max_quantity: float = 100.0
 @export var stamina_refill_per_second: float = 5.0
+#endregion
 
-func _get_configuration_warnings() -> PackedStringArray:
-	var warnings: Array[String] = []
-	if parent == null:
-		warnings.push_back("'Parent' should be defined")
-	return warnings
-
+#region Private vars
 var stamina_quantity: float
+#endregion
 
+#region Signals
+signal stamina_consumed(remaining_stamina: float)
+signal stamina_refilled(current_stamina: float)
+signal not_enough_stamina()
+#endregion
+
+#region Godot's Lifecycle
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 	
 	stamina_quantity = stamina_max_quantity
 	parent.set_meta("mds_stamina_logic", self)
+#endregion
 
+#region Business Logic
 func consume_stamina(quantity: float):
 	if stamina_quantity >= quantity:
 		stamina_quantity -= quantity
@@ -41,3 +43,12 @@ func refill_stamina_per_second():
 	
 	stamina_quantity = min(stamina_quantity + stamina_refill_per_second, stamina_max_quantity)
 	stamina_refilled.emit(stamina_quantity)
+#endregion
+
+#region Configuration Warning
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: Array[String] = []
+	if parent == null:
+		warnings.push_back("'Parent' should be defined")
+	return warnings
+#endregion
