0b576c90edc32fdb0211659fdd072141cbb806d5
mdossan <mdossan@protonmail.com>
2026-06-12 17:31:10 +0200

    ✅ Improve assertion message on up movement test

--- tests/skills/movement/test_up_movement.gd
+++ tests/skills/movement/test_up_movement.gd
@@ -3,4 +3,4 @@ extends MdsTestScene
 func test():
 	await input_press("up")
 	await wait_physics_frame()
-	assert_lt(%CharacterBody3D.position.z, 0, "CharacterBody3D has not moved forward!")
+	assert_lt(%CharacterBody3D.position.z, 0, "CharacterBody3D should have moved forward")
