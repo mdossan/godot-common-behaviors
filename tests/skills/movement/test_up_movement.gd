abaeaa623a55f38dd866be91443b2a2832bafee2
mdossan <mdossan@protonmail.com>
2026-06-12 14:52:48 +0200

    ✅ Update up movement test with new API

--- tests/skills/movement/test_up_movement.gd
+++ tests/skills/movement/test_up_movement.gd
@@ -3,9 +3,4 @@ extends MdsTestScene
 func test():
 	await input_press("up")
 	await wait_physics_frame()
-
-	if %CharacterBody3D.position.z < 0:
-		succeed("CharacterBody3D has moved forward!")
-	else:
-		fail("CharacterBody3D has not moved forward!")
-	
+	assert_lt(%CharacterBody3D.position.z, 0, "CharacterBody3D has not moved forward!")
